0cd0297d6ee0b091448aa7af849106b436324f74
warecrash <warecrash@users.noreply.github.com>
2019-11-30 16:45:55 -0600

    flags

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ Measure weekly tasks in 2.5 hour blocks
 
 
 ## Usage
+```
+tracker <command> [options]
 
-Coming soon
+Commands:
+  list
+  reset
+  start
+Options:
+  -t, --task int   The task you want to start.
+```
 
 ## Example tasks.json
 Each task requires three attributes: name, limit, and current. The name is just a string that comes up in the list of tasks. The limit is the maximum number of 2.5 hour blocks you want to spend working on that task each week. The current should be zero in your config, it is used by tracker to measure how many blocks you have completed.
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+
+	flag "github.com/spf13/pflag"
 )
 
 var saveFile string = "tasks.json"
 
 type Task struct {
 	Tasks []struct {
-		Name  string `json:"name"`
-		Limit int    `json:"limit"`
-		Current int  `json:"current"`
+		Name    string `json:"name"`
+		Limit   int    `json:"limit"`
+		Current int    `json:"current"`
 	} `json:"tasks"`
 }
 
@@ -20,17 +22,15 @@ func save(user Task) {
 	output, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println(err)
-        }
+	}
 	err = ioutil.WriteFile(saveFile, output, 0644)
 	if err != nil {
 		fmt.Println(err)
-        }
+	}
 }
 
-func getTasks(user Task) {
-
-	for index,currTask := range user.Tasks {
-
+func listTasks(user Task) {
+	for index, currTask := range user.Tasks {
 		if currTask.Limit != currTask.Current {
 			fmt.Printf("%d: %s [%d/%d]\n", index, currTask.Name, currTask.Current, currTask.Limit)
 		}
@@ -38,7 +38,8 @@ func getTasks(user Task) {
 }
 
 func resetTasks(user Task) {
-	for index,_ := range user.Tasks{
+	fmt.Printf("Resetting tasks in %s\n", saveFile)
+	for index, _ := range user.Tasks {
 		user.Tasks[index].Current = 0
 	}
 	save(user)
@@ -46,32 +47,45 @@ func resetTasks(user Task) {
 
 func startTask(user Task, id int) {
 	if user.Tasks[id].Current != user.Tasks[id].Limit {
-		fmt.Printf("Working on: %s\n",user.Tasks[id].Name)
-		user.Tasks[id].Current = user.Tasks[id].Current+1
+		fmt.Printf("Working on %s\n", user.Tasks[id].Name)
+		user.Tasks[id].Current = user.Tasks[id].Current + 1
 	} else {
 		fmt.Println("You have already met the limit for that task")
 	}
 	save(user)
 }
 func main() {
-	//id,err := strconv.Atoi(os.Args[1])
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
+	var id int
+	var op string
+	var user Task
+
+	flag.IntVarP(&id, "task", "t", 999, "The task you want to start.")
+	flag.Parse()
+	op = flag.Arg(0)
+
 	data, err := ioutil.ReadFile(saveFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-
-	var user Task
-
 	err = json.Unmarshal(data, &user)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//startTask(user, id)
-	getTasks(user)
-	//resetTasks(user)
 
+	switch op {
+	case "list":
+		listTasks(user)
+	case "reset":
+		resetTasks(user)
+	case "start":
+		if id != 999 {
+			startTask(user, id)
+			break
+		}
+		fallthrough
+	default:
+		fmt.Printf("Measure weekly tasks in 2.5 hour blocks\n\nUsage:\n  tracker <command> [options]\nCommands:\n  list\n  reset\n  start\nOptions:\n")
+		flag.PrintDefaults()
+	}
 }
